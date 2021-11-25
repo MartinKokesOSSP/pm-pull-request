3dc3763683104d2f87f155dd182dc128ae2747a7
Stepan-Kolarovsky <60381824+Stepan-Kolarovsky@users.noreply.github.com>
2021-11-25 12:39:18 +0100

    Update Mouse.php

--- classes/Mouse.php
+++ classes/Mouse.php
@@ -2,11 +2,12 @@
 
 class Mouse extends Animal
 {
+private string $fur;
 
-
-public function __construct(string $name,int $legs)
+public function __construct(string $name,int $legs,string $fur)
 {
     $this->name = $name;
     $this->legs = $legs;
+    $this->fur = $fur;
 }
 }

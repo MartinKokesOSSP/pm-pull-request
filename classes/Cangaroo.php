761a4f6ca1db2e46b8044a852829e9dadf14bcf3
Michal-Placek <michal.placek@student.ossp.cz>
2021-11-25 12:24:10 +0100

    hotovo

---
+++ classes/Cangaroo.php
@@ -0,0 +1,24 @@
+<?php
+
+class Cangaroo extends Animal
+{
+    private string $jumphigh;
+
+    public function __construct(string $name,int $legs,string $jumphigh)
+    {
+        $this->name;
+        $this->legs;
+        $this->jumphigh;
+    }
+
+
+public function getname() {
+    return $this->name;
+}
+public function getlegs() {
+    return $this->legs;   
+}
+public function getjumphigh() {
+    return $this->jumphigh;
+}
+}

95053e07fda53ab9ee07894aedc10e2eff7ecc2a
Alexsmobsenyojer <trejbal.jakub@email.cz>
2021-11-25 12:27:00 +0100

    a

---
+++ classes/Frogge.php
@@ -0,0 +1,15 @@
+<?php 
+use Animal;
+class Frogge extends Animal
+{
+public int $tongue;
+public function __construct(string $name, bool $legs, int $tongue)
+    {$this->name = $name;$this->legs = $legs;$this->tongue = $tongue;}
+public function getTongue()
+    {return $this->tongue;}
+   
+}
+
+
+
+?>

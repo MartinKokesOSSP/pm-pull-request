5815e929be36493277a050a5ffb2910bb4b71389
Alexsmobsenyojer <trejbal.jakub@email.cz>
2021-11-25 12:40:42 +0100

    zfOrMaTOvaNO

--- classes/Frogge.php
+++ classes/Frogge.php
@@ -1,15 +1,18 @@
-<?php 
+<?php
+
 use Animal;
+
 class Frogge extends Animal
 {
-public int $tongue;
-public function __construct(string $name, bool $legs, int $tongue)
-    {$this->name = $name;$this->legs = $legs;$this->tongue = $tongue;}
-public function getTongue()
-    {return $this->tongue;}
-   
+    public int $tongue;
+    public function __construct(string $name, bool $legs, int $tongue)
+    {
+        $this->name = $name;
+        $this->legs = $legs;
+        $this->tongue = $tongue;
+    }
+    public function getTongue()
+    {
+        return $this->tongue;
+    }
 }
-
-
-
-?>
